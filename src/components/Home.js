9695b031497a6cbb01f2427604049acecf54b660
fusion407 <fusion407@gmail.com>
2023-03-21 18:30:36 -0500

    create navbar and home components, new logo, some styles

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+function Home(props) {
+    return(
+        <h1>
+            Welcome! This site is a collection of
+            EDM sets/concerts. Feel free to browse and add to the
+            database.
+        </h1>
+    )
+}
+
+export default Home;
---
+++ src/components/NavBar.js
@@ -0,0 +1,70 @@
+import { NavLink } from "react-router-dom";
+import logo from '../logo.svg';
+
+
+const linkStyles = {
+    display: "inline-flex",
+    flexdirection: "row",
+    aligncontent: "center",
+    justifycontent: "center",
+    width: "5em",
+    borderStyle: "solid",
+    borderColor: "#38302E",
+    padding: "1em",
+    margin: "2em 6px 6px",
+    background: "grey",
+    textDecoration: "none",
+    color: "black",
+  };
+
+
+function NavBar(props) {
+
+    return (
+        <>
+        <div className="App-logo">
+            <img src={logo}></img>
+        </div>
+
+        <div>
+          <NavLink
+            to="/"
+            exact
+            style={linkStyles}
+            activeStyle={{
+              background: "#858da5",
+            }}
+          >
+            Home
+          </NavLink>
+          
+          
+          <NavLink
+            to="/search"
+            exact
+            style={linkStyles}
+            activeStyle={{
+              background: "#858da5",
+            }}
+          >
+            Search
+          </NavLink>
+          
+          
+          <NavLink
+            to="/add-set"
+            exact
+            style={linkStyles}
+            activeStyle={{
+              background: "#858da5",
+            }}
+          >
+            Add Set
+          </NavLink>
+          
+        </div>
+        </>
+    );
+  }
+
+export default NavBar;
