e8ec46721c4ccf136bf67fefab6afbdae3826b7f
fusion407 <fusion407@gmail.com>
2023-03-28 13:46:20 -0500

    added GET request to app component to fetch existing full set data and assigning its state

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@ import NavBar from './components/NavBar'
 import Home from './components/Home'
 import Search from './components/Search'
 import AddSet from './components/AddSet'
+import { useState, useEffect } from 'react'
 import { Route, Switch } from "react-router-dom";
 
 function App() {
+  const [fullSetData, setFullSetData] = useState()
+
+  useEffect(() => {
+    loadAllSetData();
+  }, [])
+
+  async function loadAllSetData() {
+    await fetch("http://localhost:9292/fullsets", {
+      method: "GET",
+      headers: {
+          "Content-Type" : "application/json",
+      },
+      })
+          .then((r) => r.json())
+          .then(setFullSetData)
+          .catch((error) => console.log(error))
+  }
+    
+  console.log(fullSetData)
+
   return (
     <div className="App">
       <div className="App-header">
