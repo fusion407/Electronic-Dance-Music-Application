0bb5cc3a00aa3dd0ea80447963e5e52abc9d64d8
fusion407 <fusion407@gmail.com>
2023-04-20 14:09:02 -0500

    created edit set form

--- src/components/EditSet.js
+++ src/components/EditSet.js
@@ -1,8 +1,65 @@
+import EditSetForm from "./EditSetForm"
+import { useState } from 'react'
+
 function EditSet() {
+
+    const[formData, setFormData] = useState({
+        title: '',
+        rating: '',
+        video_link: '',
+        artist_id: '',
+        event_id: '',
+        location_id: ''
+    });
+
+    async function submitSetFormData() {
+        console.log("I SUBMIT EDIT DATA")
+        // checkFormIds(formData.artist_id, formData.event_id, formData.location_id)
+        // console.log("artist id: " + artistId)
+        // console.log("event id: " + eventId)
+        // console.log("location id: " + locationId)
+        // await fetch("http://localhost:9292/fullsets", {
+        // method: "POST",
+        // headers: {
+        //     "Content-Type" : "application/json",
+        // },
+        // body: JSON.stringify({
+        //     key : formData.id,
+        //     title : formData.title,
+        //     rating : formData.rating,
+        //     video_link: formData.video_link,
+        //     artist_id : formData.artist_id,
+        //     event_id : formData.event_id,
+        //     location_id : formData.location_id,
+        // })
+        // })
+        //     .then((r) => r.json())
+        //     .then((data) =>{
+        //         setFullSetData((fullSetData) => [...fullSetData, data])
+        //     })
+        // .catch((error) => console.log(error))
+    }
+    function handleChange(e) {
+        e.preventDefault();
+        console.log(e.target.value)
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        submitSetFormData(e);
+
+    }
     return(
-        <div>
-            <h1>Edit form</h1>
-        </div>
+        <>
+            <EditSetForm 
+                handleChange={handleChange} 
+                handleSubmit={handleSubmit}
+                formData={formData}
+            />
+        </>
     )
 
 }
---
+++ src/components/EditSetForm.js
@@ -0,0 +1,88 @@
+import { Input } from "@mui/material";
+
+const formStyle = {
+    color : "#3b3a30",
+    margin : "5px"
+}
+
+function EditSetForm({formData, handleSubmit, handleChange}) {
+    return(
+        <>
+        <h1>Edit Set</h1>
+            <form className="addSetForm" onSubmit={handleSubmit}>
+                <div>
+                    <Input
+                        type="text"
+                        name="title"
+                        value={formData.title}
+                        onChange={handleChange}
+                        style={formStyle}
+                        placeholder="Title"
+                    />
+                </div>
+                <div>
+                    <Input
+                        type="text"
+                        name="rating"
+                        value={formData.rating}
+                        onChange={handleChange}
+                        style={formStyle}
+                        placeholder="Rating"
+                /> 
+                </div>
+                <div>
+                    <Input
+                        type="text"
+                        name="video_link"
+                        value={formData.video_link}
+                        onChange={handleChange}
+                        style={formStyle}
+                        placeholder="Video Link"
+                    />
+                </div>
+                <div>
+                    <Input
+                        type="text"
+                        name="artist_id"
+                        value={formData.artist_id}
+                        onChange={handleChange}
+                        style={formStyle}
+                        placeholder="Artist"
+                    />
+
+                </div>
+                <div>
+                    <Input
+                        type="text"
+                        name="event_id"
+                        value={formData.event_id}
+                        onChange={handleChange}
+                        style={formStyle}
+                        placeholder="Event"
+                    />
+
+                </div>
+                <div>
+                    <Input
+                        type="text"
+                        name="location_id"
+                        value={formData.location_id}
+                        onChange={handleChange}
+                        style={formStyle}
+                        placeholder="Location"
+                    />
+                </div>
+                <div>
+                    <Input
+                        type="submit"
+                        name="submit"
+                        style={formStyle}
+                        placeholder="Submit"
+                    />
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default EditSetForm
