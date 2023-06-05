34478db2a7f9fef38908461f002604155e4522f5
fusion407 <fusion407@gmail.com>
2023-06-05 13:51:44 -0500

    changed homepage text

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
         <>
 
             <h1>
-                This site is a database / API of
+                This application is a web API of
                 EDM (Electronic Dance Music) sets / concerts.
             </h1>
             <img src="https://edm.com/.image/t_share/MTcyNjIxMTAzOTg5MDA3NDQz/edc-las-vegas.jpg" alt="rave"/>
